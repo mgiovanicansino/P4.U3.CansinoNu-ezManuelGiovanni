684eeb7326de80428d1f1a2d7f9872c55ea35fb1
mgiovanicansino <61245131+mgiovanicansino@users.noreply.github.com>
2020-05-13 22:31:10 -0700

    commit2
    commit2

---
+++ ControlInventario/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductCode { get; set; }
+        public String ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int SupplirdId { get; set; }
+    }
+}
---
+++ ControlInventario/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public int SupplierCode { get; set; }
+        public String SupplierName { get; set; }
+        public String Email { get; set; }
+        public int Phone { get; set; }
+        public String Address { get; set; }
+    }
+}
